c71f8fa570b40a1a868e98daa7cf5f15b3d2de54
Kedar Dabhadkar <dkedar@cmu.edu>
2019-11-23 21:13:21 -0800

    GPT2 small

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 import torch
 from torch.nn import functional as F
-from pytorch_transformers import GPT2Tokenizer, GPT2Model, GPT2LMHeadModel
+from pytorch_transformers import GPT2Tokenizer, GPT2LMHeadModel
 torch.set_grad_enabled(False)
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
-model = GPT2LMHeadModel.from_pretrained('gpt2-medium').eval()
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2').eval()
 
 
-def extend(text, size=200):
+def extend(text, size=50):
     tokens = tokenizer.encode(text)
     prediction, past = torch.tensor([tokens]), None
     for i in range(size):
@@ -15,9 +15,3 @@ def extend(text, size=200):
         prediction = torch.multinomial(F.softmax(prediction[:, -1]), 1)
         tokens.append(prediction.item())
     return tokenizer.decode(tokens)
-
-
-if __name__ == "__main__":
-    test_text = 'Microsoft and Google'
-    extended = extend(test_text, 25)
-    print(extended)
