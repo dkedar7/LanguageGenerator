9f05c799982eccc825bca6e100fdd40338dab701
Kedar Dabhadkar <dkedar@cmu.edu>
2019-11-24 18:51:27 -0800

    Change Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /app
 RUN pip3 install https://download.pytorch.org/whl/cpu/torch-1.3.1%2Bcpu-cp37-cp37m-linux_x86_64.whl
 RUN pip3 install -r requirements.txt
 RUN python downloads.py
-CMD exec gunicorn main:app --bind :$PORT 
+CMD exec gunicorn main:app --bind :$PORT
--- downloads.py
+++ downloads.py
@@ -1,12 +1,20 @@
 # Downloads model(s) during docker build
-from pytorch_transformers.file_utils import cached_path
+# from pytorch_transformers.file_utils import cached_path
 
-urls = [
-    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json',
-    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt',
-    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-pytorch_model.bin',
-    'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-config.json'
-]
+# urls = [
+#     'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json',
+#     'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt',
+#     'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-pytorch_model.bin',
+#     'https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-config.json'
+# ]
 
-for url in urls:
-    cached_path(url)
+# for url in urls:
+#     cached_path(url)
+
+import joblib
+from pytorch_transformers import GPT2Tokenizer, GPT2LMHeadModel
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2').eval()
+
+joblib.dump(tokenizer, 'tokenizer.pkl')
+joblib.dump(model, 'model.pkl')
